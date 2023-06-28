3f3512a1465051b7fbd8f548f49ce3a99295ffc4
Kami <Kami@KAMI>
2023-06-28 20:18:04 +0200

    Dodaj pliki projektów.

---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,5 @@
+﻿string format = "A5";
+Prostokąt arkusz = Prostokąt.ArkuszPapieru(format);
+Console.WriteLine($"Arkusz papieru {format} ma wymiary");
+Console.WriteLine($"Bok A: {Math.Round(arkusz.BokA)} mm");
+Console.WriteLine($"Bok B: {Math.Round(arkusz.BokB)} mm");
---
+++ Zadanie 2/prostokąt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Prostokąt{
+    private decimal bokA;
+    private decimal bokB;
+
+    public decimal BokA{
+        get { return bokA; }
+        set{
+            if (!IsValidSideLength(value))
+                throw new ArgumentException("Długość boku musi być skończona i nie ujemna.");
+            bokA = value;
+        }
+    }
+
+    public decimal BokB{
+        get { return bokB; }
+        set{
+            if (!IsValidSideLength(value))
+                throw new ArgumentException("Długość boku musi być skończona i nie ujemna.");
+            bokB = value;
+        }
+    }
+
+    private static readonly Dictionary<char, decimal> wysokośćArkusza0 = new Dictionary<char, decimal>{
+        ['A'] = 1189m,
+        ['B'] = 1414m,
+        ['C'] = 1297m
+    };
+
+    public static Prostokąt ArkuszPapieru(string format){
+        if (format.Length < 2) 
+            throw new ArgumentException("Niepoprawny format arkusza.");
+
+        char klucz = format[0];
+        byte indeks;
+    
+        if (!byte.TryParse(format.Substring(1), out indeks))
+            throw new ArgumentException("Niepoprawny format arkusza.");
+
+        if (!wysokośćArkusza0.ContainsKey(klucz))
+            throw new ArgumentException("Niepoprawny format arkusza.");
+
+
+        decimal wysokośćBazowa = wysokośćArkusza0[klucz];
+        if (indeks > 10)
+            throw new ArgumentException("Niepoprawny format arkusza.");
+        decimal pierwiastekZDwóch = (decimal)Math.Sqrt(2);
+        decimal bokA = wysokośćBazowa / (decimal)Math.Pow((double)pierwiastekZDwóch, indeks);
+        decimal bokB = bokA / pierwiastekZDwóch;
+
+        return new Prostokąt(bokA, bokB);
+    }
+
+    public Prostokąt(decimal bokA, decimal bokB){
+        BokA = bokA;
+        BokB = bokB;
+    }
+
+    private bool IsValidSideLength(decimal length){
+        return length >= 0;
+    }
+}
